ddfbad6c05b33fe5fa40e954b8d871908754b1c4
Enter-vikram <kumar25n99@gmail.com>
2026-09-06 17:58:49 +0000

    Add Day 2 vector exercises

---
+++ day2/vectors.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+x = np.array([2, 3])
+y = np.array([4, 5])
+
+print("x =", x)
+print("y =", y)
+print("x + y =", x + y)
+print("x - y =", x - y)
+print("x * y =", x * y)
+print("dot product =", np.dot(x, y))
+
